72556cb84ef59f3ea05b5ee766595ccba20f96c6
FellipePatrick <fellipe.brito@ifrn.edu.br>
2025-04-06 19:10:56 -0300

    Terminando alguns casos de usos

--- src/backEnd/sic/src/main/java/com/api/sic/backend/dto/Usuario/UsuarioRequestUpdateDTO.java
+++ src/backEnd/sic/src/main/java/com/api/sic/backend/dto/Usuario/UsuarioRequestUpdateDTO.java
@@ -2,7 +2,6 @@ package com.api.sic.backend.dto.Usuario;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.ToString;
@@ -13,7 +12,6 @@ public class UsuarioRequestUpdateDTO {
     @Size(min = 3, max = 100, message = "O nome deve ter entre 3 e 100 caracteres.")
     private String nome;
     @NotBlank(message = "O telefone não pode estar em branco.")
-    @Pattern(regexp = "\\d{10,11}", message = "O telefone deve ter 10 ou 11 dígitos.")
     private String telefone;
     @NotNull(message = "A role não pode ser nula.")
     private String role; 
